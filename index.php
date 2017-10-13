78c717454af97ab84caf48395ce1ab3dccf61a4a
Jehovani Alexander Lopez Hernandez <30883620+alexlopezh@users.noreply.github.com>
2017-10-12 23:58:09 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="procesar.php" method="post" name="datospersonales">
+  Nombre:<br>
+  <input type="text" name="nombreAlumno" value="">
+  <br>
+  Apellidos:<br>
+  <input type="text" name="apellidosAlumno" value="">
+  <br>
+  Edad:<br>
+  <input type="text" name="edadAlumno" value="">
+  <br><br>
+  <input type="submit" value="Guardar">
+</form> 
+
+<p>Lopez Hernandez Jehovani Alexander</p>
+
+</body>
+</html>
---
+++ procesar.php
@@ -0,0 +1,6 @@
+<?php
+$nombre=$_POST['nombreAlumno'];
+$apellipos=$_POST['apellidosAlumno'];
+$edad=$_POST['edadAlumno'];
+echo 'Tu Nombre Es: '.$nombre.' '.$apellipos.'<Br>Edad: '.$edad;
+?>
